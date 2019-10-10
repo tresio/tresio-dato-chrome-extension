902ff11df41a619ccc467a7166444547dc77c9e9
Jesus Lizama <dev@hellojeb.us>
2019-10-10 12:36:33 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,79 @@
+(function() {
+
+  const tabStorage = {};
+  const networkFilters = {
+    urls: [
+      "*://*.datocms.com/items*",
+      "*://*.datocms.com/validate*"
+    ]
+  };
+
+  function custom_callback(data) {
+    console.log("callback", data);
+  }
+
+  chrome.webRequest.onBeforeRequest.addListener((details) => {
+    const { tabId, requestId } = details;
+    if (!tabStorage.hasOwnProperty(tabId)) {
+      return;
+    }
+
+    tabStorage[tabId].requests[requestId] = {
+      requestId: requestId,
+      url: details.url,
+      startTime: details.timeStamp,
+      status: 'pending'
+    };
+    chrome.tabs.sendMessage(tabId, {text: 'xhr_complete'}, custom_callback);
+  }, networkFilters);
+
+  chrome.webRequest.onCompleted.addListener((details) => {
+    const { tabId, requestId } = details;
+    if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+      return;
+    }
+
+    const request = tabStorage[tabId].requests[requestId];
+
+    Object.assign(request, {
+      endTime: details.timeStamp,
+      requestDuration: details.timeStamp - request.startTime,
+      status: 'complete'
+    });
+
+
+
+  }, networkFilters);
+
+  chrome.webRequest.onErrorOccurred.addListener((details)=> {
+    const { tabId, requestId } = details;
+    if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+      return;
+    }
+
+    const request = tabStorage[tabId].requests[requestId];
+    Object.assign(request, {
+      endTime: details.timeStamp,
+      status: 'error',
+    });
+  }, networkFilters);
+
+  chrome.tabs.onActivated.addListener((tab) => {
+    const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
+    if (!tabStorage.hasOwnProperty(tabId)) {
+      tabStorage[tabId] = {
+        id: tabId,
+        requests: {},
+        registerTime: new Date().getTime()
+      };
+    }
+  });
+  chrome.tabs.onRemoved.addListener((tab) => {
+    const tabId = tab.tabId;
+    if (!tabStorage.hasOwnProperty(tabId)) {
+      return;
+    }
+    tabStorage[tabId] = null;
+  });
+
+})();
---
+++ contentScript.js
@@ -0,0 +1,61 @@
+(function(){
+
+  var style = document.createElement('style');
+  style.innerHTML =
+    '[class*=tresio-counter] {' +
+      'background-color: #ffffff;' +
+      'border-radius: 100%;' +
+      'height: 26px;' +
+      'width: 26px;' +
+      'line-height: 27px;' +
+      'display: inline-block;' +
+      'border: 1px solid #cacaca;' +
+      'text-align: center;' +
+      'font-size: 11px;' +
+      'position: absolute;' +
+      'top: 7px;' +
+    '}' +
+    '.tresio-counter-o {' +
+       'right: 32px;' +
+    '}' +
+    '.tresio-counter-n {' +
+       'left: -36px;' +
+    '}';
+
+  var ref = document.querySelector('script');
+  ref.parentNode.insertBefore(style, ref);
+
+  chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.text === 'xhr_complete') {
+      let parent = document.getElementsByClassName("RichContent__content");
+      let children = [];
+      if(parent.length){
+        children = parent[0].getElementsByClassName("RichContentItem");
+        console.log(children);
+        if(children !== undefined){
+          for(let i = 1; i < children.length + 1; i++){
+
+            var child = children[i - 1];
+            var classNames = ["tresio-counter-o", "tresio-counter-n"];
+            for (let x = 0; x < classNames.length; x++){
+              if(child.getElementsByClassName(classNames[x]).length){
+                if(classNames[x] !== "tresio-counter-o") {
+                  child.getElementsByClassName(classNames[x])[0].innerHTML = i
+                }
+              } else {
+                var node = document.createElement("SPAN");
+                var textnode = document.createTextNode(i);
+                node.classList.add(classNames[x]);
+                node.appendChild(textnode);
+                child.appendChild(node);
+              }
+            }
+
+          }
+        }
+
+      }
+      sendResponse(children);
+    }
+  });
+})();
